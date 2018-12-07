e70e95615fa7b857f14e2efaea9e4a65050f923f
Dimitar Mirchev <dirchev@camplight.net>
2018-12-07 17:32:00 +0000

    fix

--- server/web-service/routes/defaultResponder.js
+++ server/web-service/routes/defaultResponder.js
@@ -1,10 +1,9 @@
-let defaultResponder = function (req, res, next) {
+let defaultResponder = function (req, res) {
   if (!res.body) {
     res.status(404).json({})
   } else {
     res.status(200).json(res.body)
   }
-  next()
 }
 
 module.exports = defaultResponder
